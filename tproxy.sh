7b6380341c23005bd408b97c92da4c1ad4b766cc
Fanju <98313711+Fanju6@users.noreply.github.com>
2026-04-05 16:18:31 +0800

    Update script version to v26.04.05

--- tproxy.sh
+++ tproxy.sh
@@ -2,7 +2,7 @@
 
 readonly SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 # Version (use YY.MM.DD format)
-readonly SCRIPT_VERSION="v26.03.28"
+readonly SCRIPT_VERSION="v26.04.05"
 
 # Configuration (modify as needed)
 
