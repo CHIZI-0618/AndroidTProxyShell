d1c06883d40835926186b63db7a71579eaa631bf
CHIZI-0618 <51962278+CHIZI-0618@users.noreply.github.com>
2026-04-07 13:46:53 +0800

    fix

--- tproxy.sh
+++ tproxy.sh
@@ -636,7 +636,7 @@ safe_chain_create() {
         cmd="ip6tables"
     fi
 
-    if ! safe_chain_exists "$family" "$table" "$chain"; then
+    if [ "$DRY_RUN" -eq 1 ] || ! safe_chain_exists "$family" "$table" "$chain"; then
         $cmd -t "$table" -N "$chain"
     fi
     $cmd -t "$table" -F "$chain"
